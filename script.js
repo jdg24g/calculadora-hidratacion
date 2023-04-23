8415ea1ba1ae0e5d5854ce1cef3830ccae743a00
jdg24g <jdg24g@gmail.com>
2023-04-23 16:53:52 -0400

    cambios menores en js

--- script.js
+++ script.js
@@ -3,33 +3,51 @@ function calHidratacionBasal(peso) {
     let sc = (peso * 4 + 7) / (peso + 90);
     let ra = sc * 1500;
     let rb = sc * 2000;
-    console.log("Multiplicado por 1500:");
-    console.log(ra);
-    console.log("Multiplicado por 2000:");
-    console.log(rb);
-    return ra,rb
-  }else {
+    return ra;
+  } else {
     if (peso <= 10) {
       let cc = peso * 100;
-      console.log(cc);
-      return cc
+      return cc;
     } else if (peso <= 20) {
       let cc = 1000 + (peso - 10) * 50;
-      console.log(cc);
-      return cc
+      return cc;
     } else if (peso < 30) {
       let cc = 1500 + (peso - 20) * 20;
-      console.log(cc);
-      return cc
+      return cc;
     } else {
-      console.log("YOU SHALL NOT PASS");
-      return cc
+      return cc;
     }
   }
 }
+
 const BOTON = document.getElementById("calcular");
 const ERR = document.getElementById("error");
+let res = document.getElementById("yun");
+const FLU = document.getElementById("flu");
+const MAN = document.getElementById("man");
 
 BOTON.addEventListener("click", () => {
-    
-})
+  const INPUT = document.getElementById("peso");
+  const PESO = INPUT.value;
+  if (PESO > 0) {
+    cpeso = +PESO;
+    res.innerText = " cc/día: " + calHidratacionBasal(cpeso);
+    flu = calHidratacionBasal(cpeso)
+    /*
+    solucionar este problema
+    mm2 = flu * 1,5 */
+    flu = flu/24
+
+    //MAN.innerText= mm2
+    FLU.innerText= flu + "cc/h"
+    ERR.style.display = "none";
+    MAN.style.display = "block";
+    FLU.style.display = "block";
+    res.style.display = "block";
+  } else {
+    ERR.style.display = "block";
+    MAN.style.display = "none";
+    FLU.style.display = "none";
+    res.style.display = "none";
+  }
+});
